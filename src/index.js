0e16ee64b2f2fd0cc50f817322af3303b1b4b834
Gumerov-Marat <59646933+Gumerov-Marat@users.noreply.github.com>
2020-03-02 22:03:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = 0) {
+  return array.length ? Math.min(...array) : 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = 0) {
+  return array.length ? Math.max(...array) : 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = exports.avg = function avg(array = 0) {
+  return array.length ? (array.reduce((sum, curr) => sum + curr)) / array.length : 0
 }
